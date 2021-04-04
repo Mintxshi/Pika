00b5d1472a70664932087ea7a89e1a11c0570143
Mintxshi <47258458+Mintxshi@users.noreply.github.com>
2021-04-04 12:58:12 -0400

    updated it

--- commands/execute.js
+++ commands/execute.js
@@ -2,14 +2,28 @@ const Discord = require('discord.js');
 console.log("execute module has started")
 
 module.exports = {
-    name: '//execute',
+    name: 'execute',
     description: 'Devs only!',
     execute(message, args){
-        const exampleEmbed = new Discord.MessageEmbed()
+        let sentence = message.content.split(" ");
+        sentence.shift();
+        sentence = sentence.join(" ");
+
+        if (!message.author.id !== 'your UID here.'){
+            eval(sentence)
+                const exampleEmbed2 = new Discord.MessageEmbed()
+                .setColor('#ff0080')
+                .setTitle("Done.")
+                .setAuthor('Pika || Developer Commands.', 'https://cdn.discordapp.com/avatars/480813065701687306/49b1ce923f007cbbddc7452bbd260963.png?size=256', 'https://pika.coffee')
+            message.channel.send(exampleEmbed2);
+            message.react('✅');
+        } else{
+            const exampleEmbed = new Discord.MessageEmbed()
             .setColor('#ff0080')
-            .setTitle("You cannot use this command or it's not out yet.")
+            .setTitle("Error 1")
             .setAuthor('Pika || Developer Commands.', 'https://cdn.discordapp.com/avatars/480813065701687306/49b1ce923f007cbbddc7452bbd260963.png?size=256', 'https://pika.coffee')
         message.channel.send(exampleEmbed);
-       //   execute(args[2])
+        message.react('❌');
+        }
     }
 }           
